0ce1f31e4e1f9281d0b5666a883c438a2ec18e53
nicolas67 <nicolas.lacombe67@gmail.com>
2015-06-28 13:19:00 +0200

    page magasin
    modif présentation page magasin
    ajout d'images de magasins

--- apps/frontend/modules/magasin/templates/indexSuccess.php
+++ apps/frontend/modules/magasin/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <div class="presentation">
-    <div class="row ma-no-h ma-no-v pa-no-h pa-no-v">
-        <div class="col-xs-8 col-xs-offset-2 pa-no-h pa-no-v">
+    <div id="menu_mag" class="menu_mag row ma-no-h pa-no-h">
+        <div class="col-xs-8 col-xs-offset-2 pa-no-h">
             <button id="bouton_invitation" type="button"><i class="fa fa-plus-circle"></i> Ajouter un magasin</button>
             <div id="menu_inv_box" class="menu_box">
                 <form method="post" action="<?php echo url_for('magasin/ajouter') ?>">
@@ -13,15 +13,27 @@
             </div>
         </div>
     </div>
-    <div class="row ma-no-h ma-no-v pa-no-h pa-no-v">
-        Mes Magasins
-        <ul class="col-xs-12">
+    <div class="row ma-no-h pa-no-h">
+        <div id="mag_perso" class="mes_mag  col-xs-5 col-xs-offset-2">
+            <h4>Mes Magasins</h4>
+            <ul class="info_mag block col-xs-12">
+                <?php
+                foreach ($magasins as $magasin) {
+                    include_partial("magasin/magasin",array("magasin"=>$magasin));
+                }
+                ?>
+            </ul>
+        </div>
+        <div id="mag_en_validation" class="mag_en_validation col-xs-3">
+            <h4>Mes Magasins en cours de validation</h4>
+            <ul class="block col-xs-12">
 
-            <?php
-            foreach ($magasins as $magasin) {
-                include_partial("magasin/magasin",array("magasin"=>$magasin));
-            }
-            ?>
-        </ul>
+                <?php
+                foreach ($magasins as $magasin) {
+                    include_partial("magasin/magasin",array("magasin"=>$magasin));
+                }
+                ?>
+            </ul>
+        </div>
     </div>
 </div>
