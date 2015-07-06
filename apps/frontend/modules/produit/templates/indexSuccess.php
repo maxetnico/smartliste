d685e229e3a8d75582176332d37d4408fe90f102
maxetnico <tutu@toto.fr>
2015-07-06 16:27:41 +0200

    ajout, sql

--- apps/frontend/modules/produit/templates/indexSuccess.php
+++ apps/frontend/modules/produit/templates/indexSuccess.php
@@ -24,39 +24,46 @@
     <div class="row ma-no-h pa-no-h">
         <div id="mag_perso" class="mes_mag block col-xs-8 col-xs-offset-2">
             <h4>Produits</h4>
-            
-            <table class="table table-hover">
-                    <tr>
-                        <th>Etat</th>
-                        <th>Catégorie</th>
-                        <th>Nom</th>
-                        <th>Partage</th>
-                        <?php if(isset($liste)) { ?><th>Quantité</th><?php } ?>
-                    </tr>
-                <?php                
-                foreach ($produits as $produit) { ?>
-                    <tr>
-                        <td width='37px'>
-                            <?php echo $produit->getEtatNom()=="VAL"?"<i class='mag_val fa fa-check-circle'></i>":($produit->getEtatNom()=="ATT"?"<i class='mag_att fa fa-clock-o'></i>":"<i class='mag_ref fa fa-times-circle'></i>"); ?>
-                        </td>
-                        <td>
-                            <?php echo $produit->getCategorieNom(); ?>
-                        </td>
-                        <td>
-                        <?php echo image_tag($produit->getImg(),array("width"=>"35px")); 
-                              echo $produit->getNom(); ?>
-                        </td>
-                        <td>
-                        <?php echo $produit->getVisibleNom(); ?>
-                        </td>
-                        <?php if(isset($liste)) { ?>
-                        <td>
-                            <input type="number" value="0">
-                        </td>
-                        <?php } ?>
-                    </tr>
-                <?php } ?>
-            </table>
+            <?php if(isset($liste)) { ?>
+            <form method="POST" action="produit/ajout">
+                <input type="hidden" name="liste" value="<?php echo $liste->getId() ?>"
+            <?php } ?>
+                <table class="table table-hover">
+                        <tr>
+                            <th>Etat</th>
+                            <th>Catégorie</th>
+                            <th>Nom</th>
+                            <th>Partage</th>
+                            <?php if(isset($liste)) { ?><th>Quantité</th><?php } ?>
+                        </tr>
+                    <?php                
+                    foreach ($produits as $produit) { ?>
+                        <tr>
+                            <td width='37px'>
+                                <?php echo $produit->getEtatNom()=="VAL"?"<i class='mag_val fa fa-check-circle'></i>":($produit->getEtatNom()=="ATT"?"<i class='mag_att fa fa-clock-o'></i>":"<i class='mag_ref fa fa-times-circle'></i>"); ?>
+                            </td>
+                            <td>
+                                <?php echo $produit->getCategorieNom(); ?>
+                            </td>
+                            <td>
+                            <?php echo image_tag($produit->getImg(),array("width"=>"35px")); 
+                                  echo $produit->getNom(); ?>
+                            </td>
+                            <td>
+                            <?php echo $produit->getVisibleNom(); ?>
+                            </td>
+                            <?php if(isset($liste)) { ?>
+                            <td>
+                                <input type="number" value="0">
+                            </td>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                </table>
+                 <?php if(isset($liste)) { ?>
+                <button class="right" type="submit">Sauvegarder</button>
+            </form>
+            <?php } ?>
         </div>
     </div>
 </div>
