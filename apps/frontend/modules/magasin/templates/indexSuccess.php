7abb7b73709f3ceb0ae39ac078195ab427e202c7
nicolas67 <nicolas.lacombe67@gmail.com>
2015-07-06 15:29:59 +0200

    sql

--- apps/frontend/modules/magasin/templates/indexSuccess.php
+++ apps/frontend/modules/magasin/templates/indexSuccess.php
@@ -6,7 +6,11 @@
                 <form method="post" action="<?php echo url_for('magasin/ajouter') ?>">
                     <span>nom : </span><input type="text" name="nommag" placeholder="Nom du magasin" value=""></br>
                     <span>image : </span><input type="text" name="lienimg" placeholder="Lien vers l'image" value=""></br>
-                    <span>Partage : </span><input type="text" name="visiblepar" placeholder="Visible par qui ?" value=""></br>
+                    <span>Partage : </span>
+                        <input type="radio" id="parmoi" name="partage" value="MOI" checked /><label for="parmoi"><i class="par_moi fa fa-user"></i> Moi</label>
+                        <input type="radio" id="parlst" name="partage" value="MOI" /><label for="parlst"><i class="par_lst fa fa-list-alt"></i> Liste</label>
+                        <input type="radio" id="parsit" name="partage" value="MOI" /><label for="parsit"><i class="par_sit fa fa-globe"></i> Site</label>
+                        </br>
                     <button type="submit" id="bouton_valider">Ajouter</button>
                     <button type="reset" class="bouton_annuler">Annuler</button>
                 </form>
@@ -14,14 +18,16 @@
         </div>
     </div>
     <div class="row ma-no-h pa-no-h">
-        <div id="mag_perso" class="mes_mag block col-xs-8 col-xs-offset-2">
-            <h4>Mes Magasins</h4>
+        <div id="mag_perso" class="mes_mag col-xs-8 col-xs-offset-2">
+            <h3>Mes Magasins</h3>
             
-            <table class="table table-hover">
+            <table class="mag entete">
                     <tr>
-                        <th>Etat</th>
-                        <th>Nom</th>
-                        <th>Partage</th>
+                        <th width='37px'>Etat</th>
+                        <th width='37px'>Nom</th>
+                        <th></th>
+                        <th width='37px'>Partage</th>
+                        <th width='37px'>Actions</th>
                     </tr>
                 <?php
                 foreach ($magasins as $magasin) {
