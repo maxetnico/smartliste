42be1bd78fc0e8c916035072de8acd6710d60579
maxetnico <tutu@toto.fr>
2015-09-07 10:02:35 +0200

    Index modifie
    liens utiles

--- apps/frontend/modules/accueil/templates/invitationSuccess.php
+++ apps/frontend/modules/accueil/templates/invitationSuccess.php
@@ -21,7 +21,7 @@
                 <form method="post" action="<?php echo url_for('accueil/inscription') ?>">
                     <span>Login : </span><input type="text" name="pseudo" placeholder="Pseudo"><br>
                     <span>Password : </span><input type="password" name="mdp" placeholder="Mot de passe"><br>
-                    <span>Mail : </span><input type="text" name="mail" placeholder="Mail (facultatif)"><br>
+                    <!--<span>Mail : </span><input type="text" name="mail" placeholder="Mail (facultatif)"><br>-->
                     <input type="hidden" name="ticket" value="<?php echo $liste->getIdPartage() ?>">
                     <button type="submit" id="bouton_inscrire">S'inscrire</button>
                     <button type="button" class="bouton_annuler">Annuler</button>
