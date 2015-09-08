3c2252789fbea93504a221882d6beaab2db8c6df
nicolas67 <nicolas.lacombe67@gmail.com>
2015-09-09 00:30:40 +0200

    Maj DB

--- smartliste.sql
+++ smartliste.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 08 Septembre 2015 à 16:41
+-- Généré le :  Mer 09 Septembre 2015 à 00:28
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `smartliste`
 --
-CREATE DATABASE IF NOT EXISTS `smartliste` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `smartliste` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `smartliste`;
 
 -- --------------------------------------------------------
@@ -63,19 +63,17 @@ INSERT INTO `categorie` (`id`, `nom`, `id_parent`, `img`, `id_utilisateur`, `id_
 
 DROP TABLE IF EXISTS `droits`;
 CREATE TABLE IF NOT EXISTS `droits` (
-  `iddroit` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` bigint(20) NOT NULL,
-  `level` tinyint(1) unsigned DEFAULT '0',
-  PRIMARY KEY (`iddroit`),
-  UNIQUE KEY `idUser` (`idUser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `iduser` bigint(20) NOT NULL,
+  `level` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `droits`
 --
 
-INSERT INTO `droits` (`iddroit`, `idUser`, `level`) VALUES
-(1, 3, 99);
+INSERT INTO `droits` (`iduser`, `level`) VALUES
+(3, 99);
 
 -- --------------------------------------------------------
 
@@ -119,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `liste` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `id_utilisateur_idx` (`id_utilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `liste`
@@ -130,7 +128,8 @@ INSERT INTO `liste` (`id`, `icone`, `nom`, `couleur`, `id_utilisateur`, `id_part
 (2, 'fa fa-ambulance', 'test', '#dc2127', 3, '4mnl2fa5pywshaq', '0000-00-00 00:00:00', NULL),
 (3, 'fa fa-ambulance', 'test2', '#dc2127', 3, 'de2g7wbc6yo3l18', '0000-00-00 00:00:00', NULL),
 (4, 'fa fa-home', 'test3', '#fbd75b', 4, '9o9orfl4km83lux', '0000-00-00 00:00:00', NULL),
-(5, 'fa fa-film', 'test', '#fbd75b', 4, 'v70ssv3uo2vidqx', '0000-00-00 00:00:00', NULL);
+(5, 'fa fa-film', 'test', '#fbd75b', 4, 'v70ssv3uo2vidqx', '0000-00-00 00:00:00', NULL),
+(6, 'fa fa-home', 'perso', '#7ae7bf', 3, 'sapvn8r8envs7dy', '2015-08-27 15:28:03', NULL);
 
 -- --------------------------------------------------------
 
@@ -154,24 +153,18 @@ CREATE TABLE IF NOT EXISTS `liste_produit_link` (
   KEY `fk_lpl_utilisateur_idx` (`coche_id_utilisteur`),
   KEY `fk_lpl_produit_idx` (`id_produit`),
   KEY `fk_lpl_liste_idx` (`id_liste`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `liste_produit_link`
 --
 
 INSERT INTO `liste_produit_link` (`id`, `id_liste`, `id_produit`, `quantite`, `id_magasin`, `coche`, `coche_date`, `coche_id_utilisteur`) VALUES
-(2, 5, 2, 3, 11, 0, NULL, NULL),
-(3, 5, 4, 2, 9, 0, NULL, NULL),
-(4, 1, 1, 1, 2, 1, '2015-08-30 01:39:14', 4),
-(5, 1, 4, 1, NULL, 1, '2015-08-30 01:39:28', 4),
-(6, 1, 2, 3, NULL, 1, '2015-08-30 01:39:25', 4),
-(7, 1, 2, 1, 2, 1, '2015-08-30 01:47:16', 4),
-(8, 1, 1, 1, 1, 1, '2015-08-30 01:47:15', 4),
-(9, 1, 7, 1, 1, 1, '2015-08-30 01:41:03', 4),
-(10, 1, 3, 1, 12, 1, '2015-09-07 09:46:44', 4),
-(11, 1, 1, 1, NULL, 1, '2015-09-07 09:46:41', 4),
-(12, 1, 7, 2, NULL, 0, NULL, NULL);
+(2, 5, 2, 3, NULL, 0, NULL, NULL),
+(3, 5, 4, 2, NULL, 0, NULL, NULL),
+(4, 6, 1, 1, 9, 0, NULL, NULL),
+(5, 6, 5, 1, 10, 0, NULL, NULL),
+(6, 1, 2, 1, NULL, 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -194,20 +187,19 @@ CREATE TABLE IF NOT EXISTS `magasin` (
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_visibilite` (`id_visibilite`),
   KEY `id_etat` (`id_etat`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Contenu de la table `magasin`
 --
 
 INSERT INTO `magasin` (`id`, `nom`, `img`, `id_utilisateur`, `id_visibilite`, `date_creation`, `nb_ajout`, `id_etat`) VALUES
-(1, 'penny', 'penny.jpg', NULL, 3, '2015-07-04 09:11:23', 0, 2),
+(1, 'penny', 'penny.jpg', 3, 1, '2015-07-04 09:11:23', 0, 1),
 (2, 'Auchan', 'auchan.jpg', 2, 3, '2015-07-05 10:11:23', 0, 1),
-(3, 'Match', 'match.jpg', 3, 1, '2015-07-06 11:11:23', 0, 1),
+(3, 'Match', 'match.jpg', 3, 3, '2015-07-06 11:11:23', 0, 1),
 (9, 'Grand Frais', 'Grand Frais.png', 3, 1, '2015-07-09 00:06:28', 0, 1),
-(10, 'Super U', 'Super U.png', 4, 3, '2015-08-29 22:53:47', 0, 1),
-(11, 'leroy merlin', 'leroy merlin.jpg', 4, 1, '2015-08-30 01:48:08', 0, 1),
-(12, 'cora tutu', 'cora tutu.jpg', 5, 2, '2015-08-30 21:11:03', 0, 1);
+(10, 'Netto', 'Netto.jpg', 3, 1, '2015-08-04 14:36:59', 0, 1),
+(11, 'test', 'test.jpg', NULL, 3, '2015-08-30 10:10:09', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -223,7 +215,7 @@ CREATE TABLE IF NOT EXISTS `magasins_favoris` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_magasin` (`id_magasin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -246,7 +238,7 @@ CREATE TABLE IF NOT EXISTS `produit` (
   KEY `fk_produit_utilisateur1_idx` (`id_utilisateur`),
   KEY `fk_produit_categorie1_idx` (`id_categorie`),
   KEY `fk_produit_visibilite_idx` (`id_visibilite`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `produit`
@@ -257,8 +249,7 @@ INSERT INTO `produit` (`id`, `nom`, `img`, `id_utilisateur`, `id_categorie`, `id
 (2, 'brosse à dent', 'produits/brosse_a_dent.jpg', NULL, 1, 1, 3),
 (3, 'lait', 'produits/lait.jpg', NULL, 1, 1, 3),
 (4, 'oeufs', 'produits/oeufs.jpg', NULL, 1, 1, 3),
-(5, 'papier wc', 'produits/pq.jpg', NULL, 1, 1, 3),
-(7, 'gateaux2', 'produits/7.jpg', 4, 1, 1, 1);
+(5, 'papier wc', 'produits/pq.jpg', NULL, 1, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -275,7 +266,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `mail` varchar(100) DEFAULT NULL,
   `datelastconn` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `utilisateur`
@@ -284,9 +275,8 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 INSERT INTO `utilisateur` (`id`, `pseudo`, `pwd`, `datecreate`, `mail`, `datelastconn`) VALUES
 (1, 'Essai1', 'totor', '2015-06-25 09:05:47', 'essai@test.com', '2015-06-26 10:52:21'),
 (2, 'm', 'kkkk', '2015-06-25 11:41:06', '', '2015-06-25 11:41:06'),
-(3, 'nico', 'aaa', '2015-06-26 11:30:43', '', '2015-09-08 13:26:48'),
-(4, 'max', 'max', '2015-07-06 10:02:38', '', '2015-09-07 09:19:33'),
-(5, 'tutu', 'tutu', '2015-08-30 21:10:03', '', '2015-08-30 21:10:03');
+(3, 'nico', 'aaa', '2015-06-26 11:30:43', '', '2015-09-08 23:56:52'),
+(4, 'max', 'max', '2015-07-06 10:02:38', '', '2015-09-08 23:56:39');
 
 -- --------------------------------------------------------
 
@@ -314,7 +304,7 @@ INSERT INTO `utilisateur_liste_link` (`id`, `id_liste`, `id_utilisateur`) VALUES
 (2, 1, 3),
 (3, 1, 4),
 (4, 5, 4),
-(5, 1, 5);
+(5, 6, 3);
 
 -- --------------------------------------------------------
 
